9b6a2ea0ccd2db59da876ddc25dcd9c2aa3c2a30
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-01-06 13:01:16 +0530

    scoreboard update and minor fixes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 turtle = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(turtle.go_up, "Up")
@@ -26,10 +27,13 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(turtle)<20:
             game_is_on = False
+            scoreboard.game_over()
 
     # detect a successful crossing
     if turtle.is_at_finish_line():
         turtle.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
+
 
 screen.exitonclick()
